203fa91f4ca1fc3ecdb3c493f4df2536063440c4
SeongJun08 <ellajune1612@gmail.com>
2024-06-16 11:47:58 +0900

    Add files via upload

---
+++ Class/main.cpp
@@ -0,0 +1,109 @@
+#include <iostream>
+
+//Encapsulation
+
+class Monster
+{
+private:
+	int mHP;
+
+public:
+	void SetHP(int hp)
+	{
+		// 지역변수 우선
+		mHP = hp;
+	}
+		
+	int GetHP() const
+	{
+		return mHP;
+	}
+};
+
+
+class Dog
+{
+public:
+
+	class Toy
+	{
+	public:
+		int color;
+		void Play();
+	};
+
+	enum Breed
+	{
+		HUSKY,
+		PUDDLE,
+	};
+	Breed mBreed;
+public:
+	void SetBreed(Breed b)
+	{
+		mBreed = b;
+	}
+};
+
+class Cat
+{
+public:
+	enum Breed
+	{
+		SHAM,
+		PERSIAN,
+	};
+
+	Breed mBreed;
+public:
+	void SetBreed(Breed b)
+	{
+		mBreed = b;
+	}
+};
+
+class Point2D
+{
+private:
+	int mX;
+	int mY;
+
+public:
+	void SetXY(int x, int y)
+	{
+		mX = x;
+		mY = y;
+	}
+
+	/*int GetX() const;
+	int GetY() const;*/
+
+	void GetXY(int& x, int& y) const
+	{
+		x = mX;
+		y = mY;
+	}
+};
+
+int main()
+{
+	/*Monster m;
+	m.SetHP(10);
+	std::cout << m.GetHP() << std::endl;*/
+
+	/*Dog d;
+	d.SetBreed(Dog::Breed::HUSKY);*/
+
+	//const Monster m{};
+	//// m.SetHP(10);
+	//std::cout << m.GetHP() << std::endl;
+
+	Point2D p1;
+	p1.SetXY(1, 1);
+
+	int x, y;
+	p1.GetXY(x, y);
+	x = 10;
+	y = 10;
+	std::cout << x << "," << y << std::endl;
+}
