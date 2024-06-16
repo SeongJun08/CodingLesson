6a79f92659811f7e10b88ece5ba7d30d1f469d32
SeongJun08 <ellajune1612@gmail.com>
2024-06-16 11:49:42 +0900

    Add files via upload

---
+++ Class/ClassEx4.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+
+class MyClass
+{
+private:
+    int *mP;
+
+public:
+    MyClass()
+    {
+        mP = new int;
+    }
+    /*
+    void Release()
+    {
+        if (mP)
+        {
+            delete mP;
+            mP = nullptr;
+        }
+    }
+    */
+
+    ~MyClass()
+    {
+        if (mP)
+        {
+            delete mP;
+            mP = nullptr;
+        }
+       
+    }
+};
+
+int main()
+{
+    MyClass c1;
+
+    //c1.Release();
+}
