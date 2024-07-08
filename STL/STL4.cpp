f2ff003bdf6127283e85645c7abfaddc56e2afe2
SeongJun08 <ellajune1612@gmail.com>
2024-07-08 09:27:41 +0900

    Update STL4.cpp

--- STL/STL4.cpp
+++ STL/STL4.cpp
@@ -7,7 +7,15 @@
 #include <forward_list> // std::forward_list
 #include <list> //std::list
 #include <deque> // std::deque
+
+#include <stack> // std::stack
+#include <queue> // std::queue, std::priority_queue
  
+#include <set> // std::set, std::multiset
+#include <map> // std::map
+
+#include <unordered_set>
+
 void ArrayEx()
 {
     // std::array
@@ -256,6 +264,195 @@ void DequeEx()
     std::cout << std::endl;
 }
 
+void StackEx()
+{
+    // 선언
+    std::stack<int> s;
+
+    // 삽입
+    s.push(1);
+    s.push(2);
+    s.push(3);
+
+    // 크기
+    std::cout << s.size() << std::endl;
+
+    // 삭제
+    s.pop();
+
+    // 가장 위에 있는 원소(LAST)
+    std::cout << s.top() << std::endl;
+
+    // 초기화식 X
+    //std::stack<int> sz{ 1,2,3,4,5 };
+
+    // 순회
+    // 범위기반 : X
+    //for (auto e : s){}
+    // 이터레이터 : X
+    //std::stack<int>::iterator;
+    // 인덱스 : X
+    //s[0];
+
+    std::stack<int> temp = s;
+    std::cout << std::endl;
+    while (!temp.empty())
+    {
+        std::cout << temp.top() << std::endl;
+        temp.pop();
+    }
+
+}
+
+void QueueEx()
+{
+    std::queue<int> q;
+
+    // 삽입
+    q.push(1);
+    q.push(2);
+    q.push(3);
+
+    // 삭제
+    q.pop();
+
+    // 크기
+    std::cout << q.size() << std::endl;
+
+    // 순회
+    while (!q.empty())
+    {
+        std::cout << q.front() << " ";
+        q.pop();
+    }
+    std::cout << std::endl;
+
+}
+
+void PriorityQueueEx()
+{
+    std::priority_queue<int> pq;    // heap data structure 제일 큰거 혹은 제일 작은것이 제일 위에 있는 구조
+
+    // 삽입
+    pq.push(5);
+    pq.push(3);
+    pq.push(4);
+    pq.push(2);
+
+    // 출력
+    std::priority_queue<int> temp = pq;
+    while (!temp.empty())
+    {
+        // 왜 top 이지?
+        std::cout << temp.top() << " ";
+        temp.pop();
+    }
+    std::cout << std::endl;
+}
+
+void SetEx()
+{
+    // unique한 값
+    // Binary Serach Tree
+    // 정렬
+    
+    // 선언
+    std::set<int> s{ 5,2,4,3,1 };
+
+    // 삽입
+    s.insert(6);
+
+    // 삭제
+    s.erase(3);
+
+    // 크기
+    std::cout << s.size() << std::endl;
+
+    // 순회
+    for (auto e : s)
+    {
+        std::cout << e << " ";
+    }
+    std::cout << std::endl;
+
+    // 검색
+    std::cout << (s.find(4) != s.end()) << std::endl;
+}
+
+void MapEx()
+{
+    // 원소 : (키 - 밸류)
+    // std::pair<T1,T2>
+    std::map<int, std::string> m{
+        {1, "one"},
+        { 2,"two" },
+        { 3,"three" },
+        { 4,"four" },
+    };
+
+    // 삽입
+    m.insert({ 5,"five" });
+    m[6] = "six";
+
+    // 크기
+    std::cout << m.size() << std::endl;
+
+    // 값에 접근
+    std::cout << m.at(2) << std::endl;
+    std::cout << m[2] << std::endl;
+
+    // { key, value }
+    // std::pair<T1, T2>
+
+    // 검색
+    std::map<int,std::string>::iterator result = m.find(1);
+    if (result != m.end())
+    {
+        // 찾음
+        // *result = std::pair<int, string>
+        //                  .first   .second
+        std::cout << "find : " << (*result).second << std::endl;
+    }
+
+    // 순회
+    // 모든 원소의 "키 : 밸류" 출력
+    // 키는 고유하나 밸류는 바꿀 수 있다
+    for (const auto& e : m)
+    {
+        // auto - std::pair<int, string>
+        // auto - const std::pair<int, string>&
+        std::cout << e.first << " : " << e.second << std::endl;
+    }
+
+    std::map<std::string, int> items
+    {
+        {"sword", 10},
+        {"hammer", 100},
+        {"shield", 50},
+        {"armor", 50},
+    };
+    std::cout << items["sword"] << std::endl;
+    items["knife"] = 30;
+    items["knife"] = 50;
+
+    ///////
+    std::multiset<int> ms;
+    std::unordered_set<int> us;
+    std::unordered_multiset<int> ums;
+
+    ms.insert(1);
+    ms.insert(2);
+    ms.insert(2);   // 중복 키
+
+    us.insert(1);
+    us.insert(2);
+    us.insert(3);
+
+    ums.insert(1);
+    ums.insert(2);
+    ums.insert(2);
+}
+
 int main()
 {
     // Sequence Container
@@ -270,4 +467,21 @@ int main()
     ListEx();
     std::cout << "----------------------" << std::endl;
     DequeEx();
+    std::cout << "----------------------" << std::endl;
+
+    // Adoptor Container
+    StackEx();
+    std::cout << "----------------------" << std::endl;
+    QueueEx();
+    std::cout << "----------------------" << std::endl;
+    PriorityQueueEx();
+    std::cout << "----------------------" << std::endl;
+
+    // Associative Container
+    SetEx();
+    std::cout << "----------------------" << std::endl;
+    MapEx();
+    std::cout << "----------------------" << std::endl;
+
+
 }
