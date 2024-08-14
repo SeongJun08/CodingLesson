1b91cf4e696cb98a5ed7aa1d3563054f0d9005a8
SeongJun08 <ellajune1612@gmail.com>
2024-08-15 02:04:18 +0900

    Create winmain.cpp

---
+++ ImageExample/winmain.cpp
@@ -0,0 +1,43 @@
+#include <Windows.h>
+#include "ImageExample.h"
+
+
+
+ImageExample myFramework;
+
+int WINAPI WinMain(
+	_In_ HINSTANCE hInstance,
+	_In_opt_ HINSTANCE hPrevInstance,
+	_In_ LPSTR lpCmdLine,
+	_In_ int nShowCmd
+)
+{
+	int ret{};
+
+	try
+	{
+		myFramework.Init(hInstance);
+
+		ret = myFramework.GameLoop();
+
+		myFramework.Release();
+	}
+	catch (const com_exception &e)
+	{
+		OutputDebugStringA(e.what());
+	}
+
+	return ret;
+
+	/*int ret{};
+
+	myFramework.Init(hInstance);
+
+	ret = myFramework.GameLoop();
+
+	myFramework.Release();
+	
+	return ret;*/
+}
+
+
