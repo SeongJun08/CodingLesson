6ed4876cc51d1f11d134945ab52a5e463e10c0cf
SeongJun08 <ellajune1612@gmail.com>
2024-08-21 22:16:35 +0900

    Update BuggyHouse.h

--- BuggyHouse/BuggyHouse.h
+++ BuggyHouse/BuggyHouse.h
@@ -4,18 +4,21 @@
 #include <list>
 #include "D2DFramework.h"
 #include "Actor.h"
+#include "Bug.h"
 
 class BuggyHouse : public D2DFramework
 {
     std::unique_ptr<Actor> mspBackground;
-    std::list<std::shared_ptr<Actor>> mBugs;
+    std::list<std::shared_ptr<Bug>> mBugs;
 
 public:
     HRESULT Init(HINSTANCE hInstance,
-        LPCWSTR title = L"D2DFramework",
+        LPCWSTR title = L"BuggyHouse",
         UINT width = 1024, UINT height = 768) override;
 
     virtual void Release() override;
     virtual void Render() override;
-};
 
+    void CheckBugs();
+
+};
