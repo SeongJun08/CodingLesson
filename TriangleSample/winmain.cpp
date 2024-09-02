0d0f3e689c12ec0bdf10be127a5f48b687b86fdf
SeongJun08 <ellajune1612@gmail.com>
2024-09-02 20:43:28 +0900

    Create winmain.cpp

---
+++ TriangleSample/winmain.cpp
@@ -0,0 +1,14 @@
+#include "TriangleSample.h"
+
+int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPSTR lpCmdLine, _In_ int nShowCmd)
+{
+	TriangleSample mySample;
+
+	mySample.Initialize(hInstance);
+
+	mySample.GameLoop();
+
+	mySample.Destroy();
+
+	return 0;
+}
