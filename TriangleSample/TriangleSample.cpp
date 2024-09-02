4f105883ef4018acba753583ef395b6fd35333d0
SeongJun08 <ellajune1612@gmail.com>
2024-09-02 20:43:42 +0900

    Create TriangleSample.cpp

---
+++ TriangleSample/TriangleSample.cpp
@@ -0,0 +1,34 @@
+#include "TriangleSample.h"
+
+
+// 0. 셰이더 준비
+//		버텍스 셰이더, 픽셀 셰이더
+//		컴파일
+
+// 1. 파이프라인 설정
+//		1-1. IA 설정
+//			버텍스 구조 ( 좌표, 색상 ... )
+//		1-2. 버텍스 셰이더, 픽셀 셰이더 커스텀 
+
+// 2. 그리기
+//		2-1. IA에 버텍스를 흘려 보냄
+
+void TriangleSample::Initialize(HINSTANCE hInstance, int width, int height)
+{
+}
+
+void TriangleSample::Destroy()
+{
+}
+
+void TriangleSample::Render()
+{
+}
+
+void TriangleSample::InitTriangle()
+{
+}
+
+void TriangleSample::InitPipeline()
+{
+}
