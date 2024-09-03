952d44c0cd201f5cd44f28c6cebcb30786a587bb
SeongJun08 <ellajune1612@gmail.com>
2024-09-03 20:29:56 +0900

    Create D3DFramework.h

---
+++ D3DSample/D3DFramework.h
@@ -0,0 +1,54 @@
+#pragma once
+
+#define WIN32_LEAN_AND_MEAM
+
+#include <string>
+#include <Windows.h>
+#include <wrl/client.h>
+#include <d3d11.h>
+
+
+class D3DFramework
+{
+public:
+	virtual void Initialize(HINSTANCE hInstance, int width = 800, int height = 600);
+	virtual void Destroy();
+	virtual void GameLoop();
+
+	LRESULT CALLBACK MessageHandle(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam);
+
+private:
+	void InitWindow(HINSTANCE hInstance);
+	void InitD3D();
+
+protected:
+	void OnResize();
+	void RenderFrame();
+
+	virtual void Render() {};
+
+private:
+	const std::wstring CLASS_NAME{ L"MyClassName" };
+	const std::wstring TITLE_TEXT{ L"Direct3D Sample" };
+
+protected:
+	int mScreenWidth{ 800 };
+	int mScreenHeight{ 600 };
+	HWND mHwnd{};
+	HINSTANCE mInstance{};
+
+	bool mMinimized{ false };
+	bool mMaximized{ false };
+	bool mResizing{ false };
+
+	Microsoft::WRL::ComPtr<ID3D11Device> mspDevice{};
+	Microsoft::WRL::ComPtr<ID3D11DeviceContext> mspDeviceContext;
+
+	Microsoft::WRL::ComPtr<IDXGISwapChain> mspSwapChain{};
+	Microsoft::WRL::ComPtr<ID3D11Texture2D> mspRenderTarget{};
+	Microsoft::WRL::ComPtr<ID3D11Texture2D> mspDepthStencil{};
+	Microsoft::WRL::ComPtr<ID3D11RenderTargetView> mspRenderTargetView;
+	Microsoft::WRL::ComPtr<ID3D11DepthStencilView> mspDepthStencilView{};
+};
+
+static LRESULT CALLBACK WindowProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam);
