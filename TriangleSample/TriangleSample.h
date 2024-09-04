c0115312a1cd609ffdaa19122cbf69c36164c50d
SeongJun08 <ellajune1612@gmail.com>
2024-09-04 22:03:54 +0900

    Update TriangleSample.h

--- TriangleSample/TriangleSample.h
+++ TriangleSample/TriangleSample.h
@@ -15,17 +15,22 @@ private:
 	void InitTriangle();
 	void InitPipeline();
 
+	HRESULT CreateTextureFromBMP();
+
 	struct VERTEX
 	{
-		FLOAT X, Y, Z;
-		FLOAT Color[4];
+		float X, Y, Z;
+		float U, V;
 	};
 
-	Microsoft::WRL::ComPtr<ID3D11VertexShader> mspVertextShader;
+	Microsoft::WRL::ComPtr<ID3D11VertexShader> mspVertexShader;
 	Microsoft::WRL::ComPtr<ID3D11PixelShader> mspPixelShader;
-	Microsoft::WRL::ComPtr<ID3D11InputLayout>mspInputLayout;
-	Microsoft::WRL::ComPtr<ID3D11Buffer> mspVertextBuffer;
-	// 버텍스 버퍼, 인덱스 버퍼
+	Microsoft::WRL::ComPtr<ID3D11InputLayout> mspInputLayout;
+	Microsoft::WRL::ComPtr<ID3D11Buffer> mspVertexBuffer;
 
+	Microsoft::WRL::ComPtr<ID3D11Texture2D> mspTexture;
+	Microsoft::WRL::ComPtr<ID3D11ShaderResourceView> mspTextureView;// 버텍스 버퍼, 인덱스 버퍼
+	Microsoft::WRL::ComPtr<ID3D11SamplerState> mspSamplerState;
+	Microsoft::WRL::ComPtr<ID3D11BlendState> mspBlendState;
 };
 
